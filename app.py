9702206638f3f88cc6f851616f4cc1543b7c00a0
Ahmad Ahmed <zcaptain2002@gmail.com>
2022-04-15 02:56:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def main():
     #
     # # Display Books
     if st.button("Predict"):
-      prediction = model.predict(dataframe)
+      prediction = model.predict(dataframedf.iloc[: , 1:])
 
       result = prediction
       st.write(result)
